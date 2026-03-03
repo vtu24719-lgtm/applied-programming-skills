4a565e7431c50abf03d3bf24124fd6b12b32026d
vtu24719-lgtm <vtu24719@veltech.edu.in>
2026-03-03 09:36:33 +0530

    Time: 0 ms (100%), Space: 43.6 MB (60.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;        // for 0s
+        int mid = 0;        // current index
+        int high = nums.length - 1; // for 2s
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
